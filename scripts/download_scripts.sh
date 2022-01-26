eb112ebaca7d2aadc67319a0c996e138ae5ddb25
Rohan <rhngla@gmail.com>
2022-01-25 19:05:44 -0800

    filtering cells

--- scripts/download_scripts.sh
+++ scripts/download_scripts.sh
@@ -11,7 +11,7 @@ get_bmark_pilot(){
     curl -o $TARGET_DIR"sample_metadata.csv" $SOURCE"sample_metadata.csv"
     curl -o $TARGET_DIR"features.tsv.gz" $SOURCE"features.tsv.gz"
     curl -o $TARGET_DIR"matrix.mtx.gz" $SOURCE"matrix.mtx.gz"
-    gdown --id "1ztNHxz5jpS0hqlW-s8iZTNIm0_4gUbhi" -O $SOURCE"metadata.csv"
+    gdown --id "1ztNHxz5jpS0hqlW-s8iZTNIm0_4gUbhi" -O $TARGET_DIR"metadata.csv"
 
     gzip -d $TARGET_DIR"features.tsv.gz"
     gzip -d $TARGET_DIR"matrix.mtx.gz"
